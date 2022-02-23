df20ceaeb37392825d9d867eb7bdddf783a92c6c
Reut Cohen <reutcohenb7@gmail.com>
2022-02-23 12:34:59 +0200

    improving code + adding winning confetti

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import BoxList from './Components/BoxList';
-
 import Box from './Components/Box';
 import { useEffect, useState } from 'react';
+import Confetti from 'react-confetti'
+
+
 
 function App() {
 
 const [boxes, setBoxes] = useState(BoxList);
 const [firstNum, setFirstNum] = useState(0);
 const [isFirstClick, setIsFirstClick] = useState(false);
-const [isDone, setIsDone] = useState(false)
+const [isDone, setIsDone] = useState(false);
+
 
   function handleRoll(){
       setBoxes(prevBoxes => {
@@ -47,17 +50,6 @@ function handleClick(id){
          return box;
       })
   });   
-is()
-}
-
-function is(){
-  let count =1;
-  boxes.map(box =>
-    box.checked === true ? count=count+1 : count
-    )
-    
-  if (count ===10)
-  setIsDone(true)
 }
 
 
@@ -74,10 +66,17 @@ function handleReset(){
   setFirstNum(0)
 }
 
-console.log(boxes)
+useEffect(() => {
+  const allHeld = boxes.every(die => die.checked)
+  const allSameValue = boxes.every(die => die.Number === firstNum)
+  if (allHeld && allSameValue) {
+      setIsDone(true)
+  }
+}, [boxes])
 
   return (
     <div className="App">
+    { isDone && <Confetti/> }
       <div className="container">
       <h1>Tenzies</h1>
       <h4>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h4>
